219157e6d9bb0eeae3eb8d530e6f83976761121c
Richard Schoen <richard@richardschoen.net>
2023-04-23 12:21:38 -0500

    Create config.py

---
+++ installmariadbpostgres/config.py
@@ -0,0 +1,16 @@
+# Put system config settings in here and include config.py in your apps
+
+[postgres]
+host=localhost
+port=5432
+user=postgres
+password=mypassword
+database=ibmidemo
+
+[mariadb]
+host=localhost
+port=3306
+user=root
+password=mypassword
+database=ibmidemo
+
