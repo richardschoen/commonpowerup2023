55d6ded0db1a563013d814da5ffe2f7e21e84ee4
Richard Schoen <richard@richardschoen.net>
2023-04-30 21:40:43 -0500

    Create custcdt.sql
    Create CUSTCDT table

---
+++ remotedatabaseaccess/custcdt.sql
@@ -0,0 +1,43 @@
+-- Create CUSTCDT Table using DDL
+-- You can also diplicate table QIWS.QCUSTCDT
+-- instead of using SQL to create CUSTCDT.
+-- CREATE TABLE QIWS.CUSTCDT (
+CREATE TABLE RPGOPNSRC.CUSTCDT (
+        CUSNUM NUMERIC(6, 0) NOT NULL DEFAULT 0 ,
+        LSTNAM CHAR(8) CCSID 37 NOT NULL DEFAULT '' ,
+        INIT CHAR(3) CCSID 37 NOT NULL DEFAULT '' ,
+        STREET CHAR(13) CCSID 37 NOT NULL DEFAULT '' ,
+        CITY CHAR(6) CCSID 37 NOT NULL DEFAULT '' ,
+        STATE CHAR(2) CCSID 37 NOT NULL DEFAULT '' ,
+        ZIPCOD NUMERIC(5, 0) NOT NULL DEFAULT 0 ,
+        CDTLMT NUMERIC(4, 0) NOT NULL DEFAULT 0 ,
+        CHGCOD NUMERIC(1, 0) NOT NULL DEFAULT 0 ,
+        BALDUE NUMERIC(6, 2) NOT NULL DEFAULT 0 ,
+        CDTDUE NUMERIC(6, 2) NOT NULL DEFAULT 0 )
+
+        RCDFMT CUSREC     ;
+
+LABEL ON TABLE RPGOPNSRC.CUSTCDT
+        IS 'PC Support Customer File' ;
+
+LABEL ON COLUMN RPGOPNSRC.CUSTCDT
+( CUSNUM TEXT IS 'Customer number field' ,
+        LSTNAM TEXT IS 'Last name field' ,
+        INIT TEXT IS 'First and middle initial field' ,
+        STREET TEXT IS 'Street address field' ,
+        CITY TEXT IS 'City field' ,
+        STATE TEXT IS 'State abbreviation field' ,
+        ZIPCOD TEXT IS 'Zip code field' ,
+        CDTLMT TEXT IS 'Credit limit field' ,
+        CHGCOD TEXT IS 'Charge code field' ,
+        BALDUE TEXT IS 'Balance due field' ,
+        CDTDUE TEXT IS 'Credit due field' ) ;
+
+GRANT SELECT
+ON RPGOPNSRC.CUSTCDT TO PUBLIC ;
+
+GRANT ALTER , DELETE , INDEX , INSERT , REFERENCES , SELECT , UPDATE
+ON RPGOPNSRC.CUSTCDT TO QSECOFR WITH GRANT OPTION ;
+
+GRANT ALTER , DELETE , INDEX , INSERT , REFERENCES , SELECT , UPDATE
+ON RPGOPNSRC.CUSTCDT TO PUBLIC WITH GRANT OPTION ;
